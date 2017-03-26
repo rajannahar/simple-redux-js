c3b2fa982c6908a2ffdf1c5f189771716c94a470
R_N101 <R_N101@msn.com>
2017-03-26 22:53:16 +0100

    06-redux-async-actions : redux asyn requests to api using axios and thunk middleware

--- src/index.js
+++ src/index.js
@@ -1,46 +1,87 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
+import axios from 'axios';
+
+const initialState = {
+	sendingRequest: false, 
+	requestReceived: false,
+	user: {
+		name: '', 
+		email: '',
+		gender: ''
+	},
+	status: '',
+	statusClass: ''
+}
 
 // REDUCER - switch statement
 // takes in state and action then returns a new state
-function counterReducer(state = { count: 0 }, action) {
 
-	const nextState = {
-		count: state.count
-	}
+function userReducer(state=initialState, action) {
+	const user = {
+		name: '',
+		email: '',
+		gender: ''
+	};
 
 	switch (action.type) {
-		case "ADD": 
-			nextState.count = state.count + 1;
-			return nextState;
+		case 'GET_USER':
+			return {
+				...state, 
+				sendingRequest: true, 
+				status: 'Pending...', 
+				statusClass: 'pending'
+			}
 			break;
-		case "MINUS": 
-			nextState.count = state.count - 1;
-			return nextState;
+		case 'USER_RECEIVED':
+			user.name = `${action.payload[0].name.first} ${action.payload[0].name.last}`
+			user.email = `${action.payload[0].email}`
+			user.gender = `${action.payload[0].gender}`
+			return {
+				...state, 
+				sendingRequest: false, 
+				user, 
+				status: 'User Received', 
+				statusClass: 'success'
+			}
 			break;
-		case "RESET": 
-			nextState.count = 0;
-			return nextState;
+		case 'ERROR':
+			return {
+				...state, 
+				sendingRequest: false, 
+				status: `${action.payload.message}`, 
+				statusClass: 'error'
+			}
 			break;
 		default:
 			return state;
 	}
-}
 
+}
 
 // STORE - initiate store
 const store = createStore(
-	counterReducer, 
-	applyMiddleware(logger), 
+	userReducer, 
+	applyMiddleware(logger, thunk), 
 	//window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
-const counterEl = document.getElementById("counter");
+
+const nameEl = document.getElementById("name");
+const emailEl = document.getElementById("email");
+const genderEl = document.getElementById("gender");
+const statusEl = document.getElementById("status");
 
 
 // RENDER function 
 function render() {
 	const state = store.getState();
-	counterEl.innerHTML = state.count.toString();
+
+	nameEl.innerHTML = state.user.name;
+	emailEl.innerHTML = state.user.email;
+	genderEl.innerHTML = state.user.gender;
+	statusEl.innerHTML = status.state;
+	statusEl.className = status.statusClass;
 }
 render(); 
 //fire render by default - returns state before any actions are dispatched
@@ -51,19 +92,32 @@ render();
 store.subscribe(render);
 
 // ACTIONS - handlers listening for action, then dispatch action
-document.getElementById("add")
-.addEventListener('click', () => {
-	store.dispatch({ type: "ADD"} );
-});
 
-document.getElementById("minus")
+document.getElementById("getUser")
 .addEventListener('click', () => {
-	store.dispatch({ type: "MINUS"} );
-});
-
-document.getElementById("reset")
-.addEventListener('click', () => {
-	store.dispatch({ type: "RESET"} );
+	store.dispatch( dispatch => {
+		// ASYN ACTION
+		// dispacth action
+		dispatch({ type: 'GET_USER' });
+		// do xhr request
+		axios.get('https://randomuser.me/api/')
+		// handle response
+		// success
+		.then(response => {
+			dispatch({ 
+				type: 'USER_RECEIVED', 
+				payload: response.data.results
+			})
+		})
+		// error
+		.catch(error => {
+			dispatch({ 
+				type: 'ERROR', 
+				payload: error
+			})
+		})
+		dispatch({ type: 'AFTER ASYNC ACTION' });
+	});
 });
 
 
