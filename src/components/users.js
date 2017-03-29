6cf7a349e7ce9fd8ddcc9ea49434804771423055
R_N101 <R_N101@msn.com>
2017-03-29 16:49:01 +0100

    11-react-router-with-redux : Link component created, test out

--- src/components/users.js
+++ src/components/users.js
@@ -2,6 +2,30 @@ import React from 'react';
 import Button from './button';
 import UserItem from './userItem';
 
+class Link extends React.Component {
+	constructor(props) {
+		super(props);
+		this.changeUrl = this.changeUrl.bind(this);
+	}
+
+	changeUrl() {
+		window.location.replace(this.props.newUrl);
+	}
+
+	render() {
+		return(
+			<div>
+				<Button
+					onClick={this.changeUrl}
+					text={`Go to ${this.props.newUrl}`}
+					className='btn btn-blue'
+				/>
+				{this.props.children}
+			</div>
+		);
+	}
+}
+
 class Users extends React.Component {
 	constructor(props) {
 		super(props);
@@ -16,6 +40,16 @@ class Users extends React.Component {
 					text={'Fetch Users'} 
 					className={'btn btn-blue'} 
 				/>
+
+				<Link newUrl='http://www.google.co.uk'>
+					<p>Go to Google UK</p>
+					<p>P tag 1</p>
+					<p>P tag 2</p>
+				</Link>
+				{/*Link Component - passing in a new URL as a property,
+									passing in a child paragraph  - rendering as we pass in 
+										 {this.props.children} into Link component
+				*/}
 
 				<div id={'users'}>
 					{data.users.map((user, i) => {
